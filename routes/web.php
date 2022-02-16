5c65e2cf1cc4a800365ad001939dcd24018b31ab
fernandogc <fernandogc@instituto-as.es>
2022-02-16 13:08:46 +0000

    rutas de ejemplo añadidas

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,27 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+Route::get('/', function () {
+    return 'Bienvenido';
+});
+
+Route::get('/usuarios', function () {
+    return "<h1>Usuarios</h1>";
+});
+
+Route::get('/usuarios/{id}', function ($id) {
+    return "<h1>Bienvenido usuario <span style='color:red'>{$id}</span></h1>";
+})->where('id', '[0-9]+');
+
+Route::get('/usuarios/nuevo', function () {
+    return "<h1>Creando nuevo usuario</h1>";
+});
+
+Route::get('/usuarios/{id}/{nombre?}', function ($id, $nombre = null) {
+    return "<h1>Bienvenido usuario <span style='color:red'>{$id}</span></h1><h2>Tu nombre es {$nombre}</h2>";
 });
